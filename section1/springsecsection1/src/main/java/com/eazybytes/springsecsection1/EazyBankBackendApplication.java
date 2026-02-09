e5c4afcc3d810de28f2f3b2e0ca2094f07567253
Yixu Pi <yixupi@mac.lan>
2026-02-09 12:10:49 -0800

    initial project setup

---
+++ section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/EazyBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springsecsection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBankBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EazyBankBackendApplication.class, args);
+    }
+
+}
