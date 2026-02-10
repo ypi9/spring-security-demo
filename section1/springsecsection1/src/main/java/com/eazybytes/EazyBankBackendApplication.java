7884ef3fb63918924a7df1e66b03854d9b1508e2
Yixu Pi <yixupi@mac.lan>
2026-02-10 10:37:50 -0800

    add other controllers and modify the main class configuration

--- section1/springsecsection1/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ section1/springsecsection1/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -1,4 +1,4 @@
-package com.eazybytes.springsecsection1;
+package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
